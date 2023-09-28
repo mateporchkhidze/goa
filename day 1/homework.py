b94e4d8610989c9b31ab9ea03103dc0319c47567
mate mate <matemarta008@gmail.com>
2023-09-29 03:56:44 +0400

    Homework 1

---
+++ day 1/homework.py
@@ -0,0 +1,22 @@
+#lets make our first homework 
+
+#info about my family and their ages
+Grandma= 65
+Grandpha= 66
+Mom= 37
+Father= 39
+total= Grandma+Grandpha+Mom+Father
+print(total)
+
+
+
+#Mission 2, How many times my age is my father's age? Now and ten years later.
+mate=16
+father=39
+division=father/mate
+print(division)
+#now lets see what happens after ten years later
+mate=26
+father=49
+division=father/mate
+print(division)
